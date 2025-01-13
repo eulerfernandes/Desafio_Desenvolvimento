fac919fdb7f8b0d43bdc9a0d003763be327a91a8
eulerfernandes <pedraeuler@gmail.com>
2025-01-13 12:49:34 -0300

    testes

---
+++ desafio5/inverter_string.js
@@ -0,0 +1,10 @@
+function inverterString(str) {
+  let resultado = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    resultado += str[i];
+  }
+  return resultado;
+}
+
+const texto = "Exemplo de string";
+console.log("Texto invertido:", inverterString(texto));
