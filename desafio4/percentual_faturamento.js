f0a7dc09d864604e6a66520690160a148f8aafb3
eulerfernandes <pedraeuler@gmail.com>
2025-01-13 12:27:48 -0300

    percentual_faturamento.js

---
+++ desafio4/percentual_faturamento.js
@@ -0,0 +1,17 @@
+const faturamentoEstados = {
+  SP: 67836.43,
+  RJ: 36678.66,
+  MG: 29229.88,
+  ES: 27165.48,
+  Outros: 19849.53,
+};
+
+const totalFaturamento = Object.values(faturamentoEstados).reduce(
+  (acc, v) => acc + v,
+  0
+);
+
+for (const estado in faturamentoEstados) {
+  const percentual = (faturamentoEstados[estado] / totalFaturamento) * 100;
+  console.log(`${estado}: ${percentual.toFixed(2)}%`);
+}
